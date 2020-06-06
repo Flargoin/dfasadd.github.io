9f9878c6af263e5a081b43c594d4f2bb89325289
Flargoin <54916933+Flargoin@users.noreply.github.com>
2020-06-06 13:37:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,48 @@ $(document).ready(function(){
     });
   });
 
+  $(document).ready(function(){
+    $('.slider-4').slick({
+      autoplay:true,
+      autoplaySpeed:3000,
+      arrows:true,
+      nextArrow:'<button type="button" class="slick-next"></button>',
+      centerMode:false,
+      slidesToShow:4,
+      slidesToScroll:1
+    });
+  });
+
+  $(document).ready(function(){
+    $('.slider-2').slick({
+      autoplay:true,
+      autoplaySpeed:3000,
+      arrows:true,
+      nextArrow:'<button type="button" class="slick-next"></button>',
+      centerMode:true,
+      slidesToShow:2,
+      slidesToScroll:1
+    });
+  });
+
+  $('.slider-123').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    centerMode: false,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-123',
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
   $(document).ready(function() {
     $('.header__burger').click(function(event){
       $('.header__burger,.header__nav').toggleClass('active');
